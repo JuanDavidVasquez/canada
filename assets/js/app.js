a9b9e788fb1a3cbc62693a8b858b00daac690e8d
Juan Vasquez <ingjuandavid@hotmail.com>
2024-10-23 14:38:33 -0500

    scrollTrigger animation scroll horizontal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,21 @@
+gsap.registerPlugin(ScrollTrigger);
+
 document.addEventListener('DOMContentLoaded', () => {
-    const leaf = new FallingLeaf('assets/img/hojaPNG.png', 'leaf-container');
-  });
-  
+    const wraps = document.querySelectorAll('.wrap');
+
+    gsap.to(".container-wrap", {
+        xPercent: -100 * (wraps.length - 1), // Mover hacia la izquierda por cada wrap
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".container-wrap",
+            pin: true,
+            scrub: 1,
+            snap: {
+                snapTo: 1 / (wraps.length - 1), // Asegurarse de que el desplazamiento se ajuste a cada sección
+                duration: 0.2,
+                delay: 0.1,
+            },
+            end: () => "+=" + (wraps.length * 100) + "%", // El ancho total del contenedor
+        }
+    });
+});
